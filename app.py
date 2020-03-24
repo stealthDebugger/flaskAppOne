c7ce7214ab4fd133ecd726441d1dd5ad6f77a159
tom ted Clang <cluberted@gmail.com>
2020-03-24 13:32:27 +0530

    change is initaial code, removed center

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return ("<center>Hello world</center>")
+    return ("Hello world")
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
